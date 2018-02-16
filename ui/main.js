43eb3ced5a390ee18e1563dea8b14fbfe40a821e
Tushar Tiwari <tushi.fire@gmail.com>
2018-02-16 11:47:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,26 +1,3 @@
-console.log('Loaded!');
-// change main text
-
-var element = document.getElementById('maintext');
-
-element.innerText = 'NEw value';
-
-
-// move image 
-
-var imageselect = document.getElementById('img');
-var marginLeft  = 0;
-function moveRight(){
-    
-    marginLeft = marginLeft + 1;
-    imageselect.style.marginLeft  = marginLeft +'px';
-}
-
-imageselect.onclick = function(){
-    var interval = setInterval(moveRight,50);
-    
-};
-
 
 //counter code
 var button = document.getElementById('counter');
