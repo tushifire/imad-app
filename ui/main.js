6d81c9a0a3b4e5a3b123503b1e5ff6ce2ad4b96f
Tushar Tiwari <tushi.fire@gmail.com>
2018-02-16 11:46:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,3 +20,18 @@ imageselect.onclick = function(){
     var interval = setInterval(moveRight,50);
     
 };
+
+
+//counter code
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function(){
+    //Make a request to the counter endpoint
+    
+    //Capture the response and store it in a variable
+    
+    //Render the variable on the correct span.
+    counter = counter + 1;
+    var span =document.geElementById('count');
+    span.innerHTML= counter.toString();
+};
