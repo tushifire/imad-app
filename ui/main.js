840c27f58ad9889f4aa2cf59e74cf934437452c8
Tushar Tiwari <tushi.fire@gmail.com>
2018-02-16 12:33:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,3 +27,14 @@ button.onclick = function(){
     request.send(null);
     
 };
+
+// Submit namee
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+  //make a request to the server and send the name
+  //Capture a list of names and render it as a list
+  
+};
