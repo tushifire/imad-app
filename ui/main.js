2a9839e980feb53c4cea38ba75cdebc6133ba33d
Tushar Tiwari <tushi.fire@gmail.com>
2018-02-16 14:14:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -63,5 +63,5 @@ submit.onclick = function(){
 // Make the request 
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
-request.open('GET','http://http://tushifire.imad.hasura-app.io/submit-name?name=' + name,true);
+request.open('GET','http://tushifire.imad.hasura-app.io/submit-name?name=' + name,true);
 request.send(null)
