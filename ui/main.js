baf6188c2324d8191d7496580d57e38bad881154
Tushar Tiwari <tushi.fire@gmail.com>
2018-02-16 12:03:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,29 @@
 
 //counter code
 var button = document.getElementById('counter');
-var counter = 0;
 button.onclick = function(){
-    //Make a request to the counter endpoint
     
+    //Create a requrest object
+    
+    var request = new XMLHttpRequest();
+
     //Capture the response and store it in a variable
+    request.onreadystatechange === function(){
+      if (request.readyState === XMLHttpRequest.DONE ){
+          //Take action
+          if(request.status === 200){
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+              
+          }
+      }
+      //Not done yet
+    };
     
     //Render the variable on the correct span.
-    counter = counter + 1;
-    var span =document.geElementById('count');
-    span.innerHTML= counter.toString();
+    //Make a request to the counter endpoint
+    request.open('GET','http://http://tushifire.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
 };
